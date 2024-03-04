ba62d827489ee16d084ba3837e7f98d3d6f023bf
Yandelf00 <zakaria.afyf00@gmail.com>
2024-03-04 21:39:44 +0100

    add

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,9 @@
+export default function taskBlock(trueOrFalse) {
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+  task = false;
+  task2 = true;
+  return [task, task2];
+}
